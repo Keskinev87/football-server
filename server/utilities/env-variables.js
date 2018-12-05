aae91b301e35e1bf20e7db23515e58927a81f777
Yavor Keskinev <yavor_keskinev@abv.bg>
2018-12-06 00:41:06 +0200

    Update env-variables.js

--- server/utilities/env-variables.js
+++ server/utilities/env-variables.js
@@ -5,7 +5,7 @@ module.exports = {
         'X-Auth-Token': 'f8a83daa19804e2a966103601127b9b5',
         'Content-Type': 'application/json'
     },
-    // competitions: "2013,2016,2021,2001,2018,2015,2002,2019,2003,2017,2014,2000"
-    competitions:"2002"
+    competitions: "2013,2016,2021,2001,2018,2015,2002,2019,2003,2017,2014,2000"
+    // competitions:"2002"
 
 }
